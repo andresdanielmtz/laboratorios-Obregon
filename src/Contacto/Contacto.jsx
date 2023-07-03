2ee56af76f9d23b82e5577281fbf99f9ba98947f
Andrés Martínez <autummata@gmail.com>
2023-07-03 15:47:55 -0700

    Agregado Mapa, actualizado Contactanos

--- src/Contacto/Contacto.jsx
+++ src/Contacto/Contacto.jsx
@@ -1,16 +1,22 @@
-import "./Contacto.css"
+import "./Contacto.css";
 
 export default function Contacto() {
-    return (
-        <div className="contacto">
-            <h1>Contacto</h1>
-            <p>Correo:
-                <a href="mailto:" target="_blank" rel="noopener noreferrer">
-
-                </a>
-            </p>
-        </div>
-    );
+  return (
+    <div className="contacto">
+      <h2 id="contact-title"> Contactanos </h2>
+      <h3> Correo </h3>
+      <p> lab-obregon@hotmail.com </p>
+      <h3> Telefono </h3>
+      <div id="contacto-phones">
+        <ul>
+          <li> 645 104 0346 (Teléfono y Whatsapp)</li>
+          <li> 645 332 2791 </li>
+          <li> 645 332 5362 </li>
+        </ul>
+      </div>
+      <h3> Horario de Atencion </h3>
+      <p> Lunes a Viernes: 7:30am - 5pm </p>
+     <p> Sabado: 8am - 1pm </p>
+    </div>
+  );
 }
-
-    
--- src/Donde/Donde.jsx
+++ src/Donde/Donde.jsx
@@ -1,11 +1,57 @@
-import "./Donde.css";  
+import "./Donde.css";
+import React, { useRef, useEffect } from "react";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import Overlay from "ol/Overlay";
+import { fromLonLat, transform } from "ol/proj.js";
+import "ol/ol.css";
 
 export default function Donde() {
-    return (
-        <div className="donde">
-            <h1> Donde estamos</h1>
-        </div>
-    );
-}
+  const mapRef = useRef(null);
 
-    
+  /* coords from google maps to openlayer */
+  const coords = [-110.3048252, 30.984385]; // real coordinates
+  const real = transform(coords, "EPSG:4326", "EPSG:3857");
+
+  useEffect(() => {
+    const map = new Map({
+      target: mapRef.current,
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: real,
+        zoom: 18,
+      }),
+    });
+
+    let division = document.createElement("div");
+    division.innerHTML =
+      '<img click = {console.log(":)} src="https://cdn.discordapp.com/attachments/1011429768740208671/1125557550436331641/pointer.png"/>';
+
+    let jsonOverlay = new Overlay({
+      position: real,
+      positioning: "center-center",
+      element: division,
+      stopEvent: true,
+    });
+
+    map.addOverlay(jsonOverlay);
+  }, []);
+  return (
+    <div className="donde">
+      <h1> Donde estamos</h1>
+      <div
+        style={{ height: "70%", width: "80%" }}
+        id="location-map"
+        ref={mapRef}
+      ></div>
+      <p>
+        Avenida Obregón #77, Colonia Centro <br /> <i> Cananea, Sonora </i>
+      </p>
+    </div>
+  );
+}
