6d60a6e04f73412f96e2ac85c9d457488b4b3d8a
Andrés Martínez <autummata@gmail.com>
2023-07-05 14:30:13 -0700

    Added background image

--- src/QueSomos/QueSomos.jsx
+++ src/QueSomos/QueSomos.jsx
@@ -4,9 +4,9 @@ import Scientist from "../assets/big_circle.jpg";
 export default function QueSomos() {
   return (
     <div className="QueSomos">
-        <div id="QueSomos-img">
-            <img src={Scientist} alt="Scientist" id = "QueSomos-spImg"/>
-        </div>
+      <div id="QueSomos-img">
+        <img src={Scientist} alt="Scientist" id="QueSomos-spImg" />
+      </div>
 
       <div id="QueSomos-content">
         <h2 id="QueSomos-title"> ¿Quiénes Somos? </h2>
@@ -14,9 +14,9 @@ export default function QueSomos() {
           {" "}
           Somos un laboratorio clínico especializado que brinda una amplia gama
           de servicios de alta calidad para satisfacer las necesidades de
-          nuestros clientes. Nuestro compromiso es ofrecer diagnósticos
-          precisos y confiables, respaldados por un equipo de profesionales
-          altamente capacitados y tecnología de vanguardia.{" "}
+          nuestros clientes. Nuestro compromiso es ofrecer diagnósticos precisos
+          y confiables, respaldados por un equipo de profesionales altamente
+          capacitados y tecnología de vanguardia.{" "}
         </p>
 
         <div id="services-list">
@@ -38,6 +38,8 @@ export default function QueSomos() {
               {" "}
               <b> Anti-doping </b>{" "}
             </li>
+            <li> Servicios a empresas y público en general. </li>
+            <li> Servicios a instituciones gubernamentales (ISSTE). </li>
           </ul>
         </div>
       </div>
--- src/Title/Title.jsx
+++ src/Title/Title.jsx
@@ -1,10 +1,14 @@
-import "./Title.css" 
-import Logo from "../assets/logo.png"
+import "./Title.css";
+import Logo from "../assets/logo.png";
+import Landscape from "../assets/upscaleLandscape.jpeg";
 export default function Title() {
-    return (
-        <div className="title">
-            <img src={Logo} alt="Logo" id = "title-logo"/>
-            <h4 id = "title-subtitle">Ubicado en Cananea, Sonora</h4>
-        </div>
-    );
+  return (
+    <div className="title">
+      <div className="title-bg">
+        {/* <img src={Landscape} alt="Logo" id = "title-background"/> */}
+        <img src={Logo} alt="Logo" id="title-logo" />
+        <h4 id="title-subtitle">Ubicado en Cananea, Sonora</h4>
+      </div>
+    </div>
+  );
 }
